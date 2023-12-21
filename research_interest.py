bd44a422a84b4e7831d39d43508b0bdebf135e08
TongmengXie <ucfntxi@ucl.ac.uk>
2023-12-21 18:13:13 +0000

    first commit

---
+++ research_interest.py
@@ -0,0 +1,99 @@
+import streamlit as st
+
+
+
+st.title('Researches- Tongmeng Xie')
+st.write('# Address Matching and Entity Extraction Across Data Sets')
+
+with st.columns(3)[1]:
+    st.image('./Overview_addrmatch.png', caption='Overview of Address Matching Workflow')
+    st.image('./Conc_ensemble_lev.png', caption='Conclusions on various ensemble levels')
+    st.image('./Comp_Ensemble.png', caption='Comparison of different Ensemble methods')
+    st.image('./Prep_NER_train.png', caption='Preparing for NER training process')
+
+
+other_researches =  \
+'''
+# **Has the expansion of Ultra Low Emission Zone in 2021 improved air quality in London? How to quantify the improvement?**
+
+*The study found that the expansion of London's Ultra Low Emission Zone (ULEZ) in October 2021 led to a modest 3-9% reduction in PM10-associated pollutants, confirmed causality via Regression Discontinuity Design (RDD), and noted spatial spillover effects, but suggests further analysis on other pollutants and time scope adjustments.*
+
+## **Research Questions:**
+
+1.  Did the expansion of the Ultra Low Emission Zone (ULEZ) in October 2021 improve air quality in London?
+
+2.  How can this improvement be quantified?
+
+## **Key Findings:**
+
+### **Air Quality Improvement:**
+
+The expansion of ULEZ on October 25, 2021, led to a noticeable but not significant improvement in air quality. Specifically, there was a 3-9% reduction in PM10-associated pollutants in Greater London.
+
+### **Sub-Questions:**
+
+1.  **Categorization of Pollutants**: Pollutants were organized into groups based on their collinearity. The categories include:
+
+    -   NOx (NO, NO2, NOXasNO2)
+
+    -   PM10 & Associated (PM10, NV10, V10)
+
+    -   PM2.5_Associated (NV2.5, V2.5, AT2.5, AP2.5)
+
+    -   SO2 (SO2, AP10, CO)
+
+    -   Uncategorised: PM2.5, O3
+
+2.  **Causality**: The study confirmed a causal relationship between ULEZ expansion and air quality improvement through Regression Discontinuity Design (RDD).
+
+3.  **Spatial Patterns**: Air quality improved not just within the expanded ULEZ area but also in areas outside it, potentially due to spatial spillover effects.
+
+### **Areas for Further Research:**
+
+1.  **Other Pollutants**: Further investigation is needed to understand the impact on other pollutants.
+
+2.  **Average Performance Metrics**: A more comprehensive measure of air quality improvement is yet to be developed.
+
+3.  **Time Scope**: The timeframe for assessing the effects of ULEZ expansion may need adjustment and could necessitate iterative analysis.
+
+# Why are KS4 performance in Liverpool and Manchester lower than the national average? Exploration and Quantification of Socio-economic Factors Influencing KS4 Performance in England
+
+*The study reveals that while there is no significant difference in overall deprivation between Liverpool & Manchester and the rest of England, students in Liverpool & Manchester are more socio-demographically disadvantaged, and these disadvantages have a greater negative impact on their educational outcomes.*
+
+## **Main Research Questions:**
+
+1.  How does the attainment 8 score relate to deprivation indicators?
+
+2.  Are socio-economic disadvantages more severe in Liverpool & Manchester?
+
+3.  Do socio-economic disadvantages have a greater impact on educational outcomes in Liverpool & Manchester?
+
+## **Key Findings:**
+
+### **Relationship between Attainment 8 and Deprivation**
+
+-   After accounting for deprivation-related variables, the model's explanatory power (Adj. *R*2) increased from 57.5% to 77.3%.
+
+-   An additional 1% of students with Special Education Needs (SEN) is associated with a 48.3-point decrease in a borough's attainment 8 score.
+
+### **Hypothesis Tests**
+
+1.  **Severity of Socio-Economic Disadvantages in Liverpool & Manchester**: There is no significant difference in the level of deprivation in and outside of Liverpool & Manchester.
+
+2.  **Impact of Socio-Economic Disadvantages in Liverpool & Manchester**: Socio-demographic disadvantages are more heavily weighted in Liverpool & Manchester, affecting educational outcomes more than they do in the rest of England.
+
+### **Model Limitations**
+
+-   One variable related to socio-demographics in Liverpool & Manchester had a p-value greater than 0.05, indicating the model may not be a perfect fit.
+
+-   The study may benefit from the inclusion of other socio-economic factors like disability, or the use of machine learning techniques for better fit.
+
+## **Discussion**
+
+Students in Liverpool & Manchester are more socio-demographically disadvantaged, and this has a heavier impact on their educational outcomes than for students outside these areas. Interventions such as cultural competency training for educators and safe spaces for marginalized students could improve educational equity and inclusion in Liverpool & Manchester.
+
+By understanding these factors, policymakers can better tailor interventions to support students from disadvantaged backgrounds.
+'''
+
+st.write(other_researches)
+
